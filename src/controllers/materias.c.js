64542d62966c5f29317117a0c7710d0d2348e1fd
jesusSalazar14 <salazargodoyja@uvm.edu.ve>
2024-03-15 14:21:07 -0400

    Funciones de Materias

--- src/controllers/materias.c.js
+++ src/controllers/materias.c.js
@@ -1,7 +1,118 @@
-const materiasModel = require('../models/materias.m.js')
+const materiasModel = require("../models/materias.m.js");
+const profesoresModel = require("../models/profesores.m.js");
 
 class materiasControllers {
-    
+  async listar() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const datos = await materiasModel.find();
+        if (!datos) {
+          return reject("No hay Servicios Registrados");
+        }
+        resolve(datos); //enviamos respuesta de la promesa
+      } catch (error) {
+        return reject(error);
+      }
+    });
+  }
+
+  async listarUna(id) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const dato = await materiasModel.findById(id);
+        if (dato) {
+          return resolve(dato);
+        }
+        reject("No existe la materia que estas buscando"); //enviamos respuesta de la promesa
+      } catch (error) {
+        return reject(error);
+      }
+    });
+  }
+
+  async agregar(materia) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const verificacionExiste = await materiasModel.find({nombre: materia.nombre}); // Validamos que no se repitan las materias
+        if (verificacionExiste.length > 0) {
+            return reject("Ya existe una materia con ese nombre");
+        }
+        const verificiacionProfesor= await profesoresModel.findById(materia.profesorId); // Validamos que exista el profesor
+        if (!verificiacionProfesor) {
+            return reject("No existe el profesor que dará esta materia")
+        }
+        const data = {
+            nombre: materia.nombre,
+            descripcion: materia.descripcion,
+            profesorId: materia.profesorId,
+            creditos: Number(materia.creditos)
+        } // Creamos el documento con los tipos de datos correctos
+        const datos = await materiasModel.create(data);
+        if (datos) {
+            return resolve(datos)
+        }
+        return reject("No se pudo agregar la materia")
+      } catch (error) {
+        return reject(error);
+      }
+    });
+  }
+
+  async actualizar(id, materia) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const verificacionExisteId = await materiasModel.findById(id); // Validamos que exista la materia a editar
+        if (!verificacionExisteId) {
+            return reject("No existe la materia")
+        }
+        const verificacionExiste = await materiasModel.find({nombre: materia.nombre}); // Validamos que no se repitan las materias
+        if (verificacionExiste.length > 1) {
+            return reject("Ya existe una materia con ese nombre");
+        }
+        const verificiacionProfesor= await profesoresModel.findById(materia.profesorId); // Validamos que exista el profesor
+        if (!verificiacionProfesor) {
+            return reject("No existe el profesor que dará esta materia")
+        }
+        const data = {
+            nombre: materia.nombre,
+            descripcion: materia.descripcion,
+            profesorId: materia.profesorId,
+            creditos: Number(materia.creditos)
+        } // Creamos el documento con los tipos de datos correctos
+        const datos = await materiasModel.findByIdAndUpdate(id, data);
+        if (datos) {
+            return resolve({
+                _id: datos._id,
+                nombre: materia.nombre,
+                descripcion: materia.descripcion,
+                profesorId: materia.profesorId,
+                creditos: Number(materia.creditos)
+            })
+        }
+        return reject("No se pudo editar la materia")
+      } catch (error) {
+        return reject(error);
+      }
+    });
+  }
+
+  async eliminar(id) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const verificacionExisteId = await materiasModel.findById(id); // Validamos que exista la materia a eliminar
+        if (!verificacionExisteId) {
+            return reject("No existe la materia")
+        }
+        const datos = await materiasModel.findByIdAndDelete(id); // Eliminamos la materia
+        if (datos) {
+            return resolve(datos)
+        }
+        return reject("No se pudo eliminar la materia")
+      } catch (error) {
+        return reject(error);
+      }
+    });
+  }
 }
 
 module.exports = new materiasControllers();
